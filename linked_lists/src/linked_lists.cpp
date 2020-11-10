b6f2e07d58799d73f0c35134b8b549c5bee27785
Marin Bilić <tehflagcarrier@gmail.com>
2020-11-10 20:32:13 +0100

    initial commit

---
+++ linked_lists/src/linked_lists.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// Name        : linked_lists.cpp
+// Description : Linked list test.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
